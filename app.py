dfafb3935dd31a2b1885aed0978ae1384507df7e
Bryan Juarez Limon <bryan.juarez@uabc.edu.mx>
2025-07-19 18:58:00 -0700

    Fix

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 
 # importa desde el paquete agendaback
-from models import db
-from routes.auth import auth_bp
-from routes.prospect import prospect_bp
-from routes.appointment import appointment_bp
-from routes.call import call_bp
-from routes.history import history_bp
+from .models import db
+from .routes.auth import auth_bp
+from .routes.prospect import prospect_bp
+from .routes.appointment import appointment_bp
+from .routes.call import call_bp
+from .routes.history import history_bp
 
 def create_app():
     app = Flask(__name__)
