942637e8cbc266d5e5b173164bacad9ae454bcd4
Bryan Juarez Limon <bryan.juarez@uabc.edu.mx>
2025-07-19 19:11:53 -0700

    fix

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from .routes.history import history_bp
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.from_object('.config.Config')
 
     CORS(app, resources={r"/*": {"origins": "*"}})
     JWTManager(app)
